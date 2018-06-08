14f4a48b1b4aacc9b09d200362f70747d4eb4f8e
Sutariya Parixit <32614362+Bhai4You@users.noreply.github.com>
2018-06-08 07:07:27 +0000

    Add files via upload

---
+++ requirement.sh
@@ -0,0 +1,3 @@
+z="
+";Iz='-y';Pz='et -';Sz='ash-';Vz='e';Nz='js -';Kz='inst';Gz='upgr';Mz='node';Uz='scat';Bz='r';Oz='figl';Tz='obfu';Ez='te -';Cz='apt ';Fz='y';Jz='pkg ';Hz='ade ';Qz='npm ';Lz='all ';Dz='upda';Az='clea';Rz='-g b';
+eval "$Az$Bz$z$Cz$Dz$Ez$Fz$z$Cz$Gz$Hz$Iz$z$Jz$Kz$Lz$Mz$Nz$Fz$z$Cz$Kz$Lz$Oz$Pz$Fz$z$Qz$Kz$Lz$Rz$Sz$Tz$Uz$Vz$z$Az$Bz"
