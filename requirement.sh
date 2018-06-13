51222871b45366abe366b8e41f4299cc0c10b934
Sutariya Parixit <32614362+Bhai4You@users.noreply.github.com>
2018-06-13 14:28:41 +0000

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -1,3 +1,3 @@
 z="
-";Iz='-y';Pz='et -';Sz='ash-';Vz='e';Nz='js -';Kz='inst';Gz='upgr';Mz='node';Uz='scat';Bz='r';Oz='figl';Tz='obfu';Ez='te -';Cz='apt ';Fz='y';Jz='pkg ';Hz='ade ';Qz='npm ';Lz='all ';Dz='upda';Az='clea';Rz='-g b';
-eval "$Az$Bz$z$Cz$Dz$Ez$Fz$z$Cz$Gz$Hz$Iz$z$Jz$Kz$Lz$Mz$Nz$Fz$z$Cz$Kz$Lz$Oz$Pz$Fz$z$Qz$Kz$Lz$Rz$Sz$Tz$Uz$Vz$z$Az$Bz"
+";Fz='y';Cz='apt ';Gz='upgr';Kz='inst';Mz='node';Az='clea';Jz='pkg ';Iz='-y';Bz='r';Xz='e';Ez='te -';Lz='all ';Oz='figl';Sz='npm ';Wz='scat';Dz='upda';Uz='ash-';Qz='curl';Nz='js -';Hz='ade ';Vz='obfu';Rz=' -y';Pz='et -';Tz='-g b';
+eval "$Az$Bz$z$Cz$Dz$Ez$Fz$z$Cz$Gz$Hz$Iz$z$Jz$Kz$Lz$Mz$Nz$Fz$z$Cz$Kz$Lz$Oz$Pz$Fz$z$Cz$Kz$Lz$Qz$Rz$z$Sz$Kz$Lz$Tz$Uz$Vz$Wz$Xz$z$Az$Bz"
